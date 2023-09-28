5a746978812ef06f25e581fa37e0bd5c37b44e10
Mathiyazhahan Maheswaran <36535996+mmaheswa@users.noreply.github.com>
2023-09-28 21:54:39 +0530

    Update googleEmailCheck.py

--- googleEmailCheck.py
+++ googleEmailCheck.py
@@ -1,10 +1,11 @@
+import argparse
 import csv
 import dns.resolver
 
 # Function to check if the domain is using Google Workspace
 def is_google_workspace_domain(domain):
     try:
-        mx_records = dns.resolver.query(domain, 'MX')
+        mx_records = dns.resolver.resolve(domain, 'MX')
         for mx in mx_records:
             if "google.com" in str(mx.exchange):
                 return "Yes"
@@ -12,13 +13,19 @@ def is_google_workspace_domain(domain):
     except dns.resolver.NXDOMAIN:
         return "No"
 
-# Input and output file paths
-input_file_path = "GoogleEmail.csv"
-output_file_path = "output.csv"
+# Create argument parser
+parser = argparse.ArgumentParser(description="Check if email addresses are hosted on Google Workspace.")
+
+# Add input and output file arguments
+parser.add_argument("input_file", help="Path to the input CSV file")
+parser.add_argument("output_file", help="Path to the output CSV file")
+
+# Parse command-line arguments
+args = parser.parse_args()
 
 # Open the input CSV file for reading and the output CSV file for writing
-with open(input_file_path, 'r', newline='') as input_file, \
-     open(output_file_path, 'w', newline='') as output_file:
+with open(args.input_file, 'r', newline='') as input_file, \
+     open(args.output_file, 'w', newline='') as output_file:
 
     # Create CSV reader and writer objects
     csv_reader = csv.reader(input_file)
@@ -27,10 +34,7 @@ with open(input_file_path, 'r', newline='') as input_file, \
     # Read the header row from the input file
     header = next(csv_reader)
 
-    # Add a new column header for Google Workspace status
-    header.append("Google Workspace")
-
-    # Write the updated header to the output file
+       # Write the updated header to the output file
     csv_writer.writerow(header)
 
     # Iterate through the rows in the input CSV
@@ -45,4 +49,4 @@ with open(input_file_path, 'r', newline='') as input_file, \
         # Write the updated row to the output file
         csv_writer.writerow(row)
 
-print(f"Results written to {output_file_path}")
+print(f"Results written to {args.output_file}")
